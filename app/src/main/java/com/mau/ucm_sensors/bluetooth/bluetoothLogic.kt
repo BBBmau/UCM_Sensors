0c17382190b21a1be954e63c84b637f732c5e2dd
Mau <alvarez.mauriciotm@gmail.com>
2021-10-02 16:43:29 -0700

    Add Bluetooth/Location Permission Request

--- app/src/main/java/com/mau/ucm_sensors/bluetooth/bluetoothLogic.kt
+++
@@ -1,7 +0,0 @@
-package com.mau.ucm_sensors.bluetooth
-
-// Model for bluetooth capability
-
-class bluetoothLogic {
-
-}
--- app/src/main/java/com/mau/ucm_sensors/bluetooth/model/BluetoothLogic.kt
+++ app/src/main/java/com/mau/ucm_sensors/bluetooth/model/BluetoothLogic.kt
@@ -1,39 +1,29 @@
-package com.mau.ucm_sensors
+package com.mau.ucm_sensors.bluetooth.model
 
 import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.mau.ucm_sensors.bluetooth.model.bluetoothLogic
-import java.security.AccessController.getContext
 
-class MainActivity : AppCompatActivity() {
-    private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
+// Model for bluetooth capability
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        checkPermission()
+object bluetoothLogic :AppCompatActivity() {
 
+    fun checkPermission(state: Context, activity : Activity){
 
-    }
-
-    private fun checkPermission(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-            if(ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)
+            if(ContextCompat.checkSelfPermission(state,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION)
                 != PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN),
-                    0)
+                    activity,
+                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                    1)
                 Log.d("BluetoothLogic","Permission was not Granted")
             }
         }
@@ -46,12 +36,11 @@ class MainActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         Log.d("BluetoothLogic", "In onRequestPermissionsResult")
-        if (requestCode == 0 && grantResults.isNotEmpty()){
+        if (requestCode == 1 && grantResults.isNotEmpty()){
             for(i in grantResults.indices){
                 if(grantResults[i] == PackageManager.PERMISSION_GRANTED)
                     Log.d("Permission Request", "${permissions[i]} is granted")
             }
         }
     }
-
 }
