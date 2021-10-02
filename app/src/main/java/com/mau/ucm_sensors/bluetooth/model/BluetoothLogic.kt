e5f1824e77d22e055f18521e0c36d75e1bbfd7ff
Mau <alvarez.mauriciotm@gmail.com>
2021-10-02 16:44:17 -0700

    Reset BluetoothLogic.kt

--- app/src/main/java/com/mau/ucm_sensors/bluetooth/model/BluetoothLogic.kt
+++ app/src/main/java/com/mau/ucm_sensors/bluetooth/model/BluetoothLogic.kt
@@ -12,35 +12,8 @@ import androidx.core.content.ContextCompat
 
 // Model for bluetooth capability
 
-object bluetoothLogic :AppCompatActivity() {
+object BluetoothLogic :AppCompatActivity() {
 
-    fun checkPermission(state: Context, activity : Activity){
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-            if(ContextCompat.checkSelfPermission(state,
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-                != PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(
-                    activity,
-                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-                    1)
-                Log.d("BluetoothLogic","Permission was not Granted")
-            }
-        }
-    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        Log.d("BluetoothLogic", "In onRequestPermissionsResult")
-        if (requestCode == 1 && grantResults.isNotEmpty()){
-            for(i in grantResults.indices){
-                if(grantResults[i] == PackageManager.PERMISSION_GRANTED)
-                    Log.d("Permission Request", "${permissions[i]} is granted")
-            }
-        }
-    }
 }
